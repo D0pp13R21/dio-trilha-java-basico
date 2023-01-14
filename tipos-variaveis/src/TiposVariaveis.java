1cb8315338937ec16609c260b8fdc64ca7468e6e
D0pp13R21 <marinho.dev98@gmail.com>
2023-01-14 15:43:55 -0300

    Conhecendo "final" em java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short x = 1;
+        int y = x;
+        short z = (short) y;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
