7f40f4068a4282a3ccd171654792d700846f62c9
D0pp13R21 <marinho.dev98@gmail.com>
2023-01-14 16:03:32 -0300

    Incremento

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,5 +3,15 @@ public class Operadores {
         String nomeCompleto = "LINGUAGEM " + "JAVA";
 
         System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        numero += -2;
+
+        System.out.println(numero);
     }
 }
